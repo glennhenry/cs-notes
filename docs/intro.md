7efae03fb20f4c98e10e27ec1e1f9706f7a1880a
glennhenry <glennhenrybdg05@gmail.com>
2023-07-18 21:19:45 +0700

    First Commit!!!

---
+++ docs/intro.md
@@ -0,0 +1,13 @@
+---
+slug: /
+id: home
+title: Home
+description: CS Notes
+---
+# CS Notes 
+
+My Computer Science notes from various online courses, tutorial, and lectures. I wanted to make my notes into a documentation-style layout to improve the overall readability and presentation.
+
+All topics:
+- [Computer Graphics](gaming)
+- [Frontend Web Development](gaming2)
---
+++ docusaurus.config.js
@@ -0,0 +1,69 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'CS Notes',
+  tagline: 'My CS notes from various online course, tutorial and lectures',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-test-site.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  organizationName: 'glennhenry',
+  projectName: 'computer-science-notes',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/', 
+          sidebarPath: require.resolve('./sidebars.js')
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Docs',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        }
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
