47b65b1c90e8f11bbe32b745ce17d6c11ca66874
glennhenry <glennhenrybdg05@gmail.com>
2024-03-10 15:36:18 +0700

    update copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,7 +103,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()}, myself. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
