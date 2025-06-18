ffe713589e17859c7943e5701b9ec403d6dc09b7
glennhenry <glennhenrybdg05@gmail.com>
2025-06-18 22:58:30 +0700

    Increase spacing and change font family

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,6 +26,30 @@ export default defineConfig({
   },
   integrations: [
     starlight({
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "preconnect",
+            href: "https://fonts.googleapis.com",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossorigin: "",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap",
+          },
+        },
+      ],
       plugins: [starlightThemeObsidian()],
       customCss: ["./src/assets/custom.css"],
       logo: {
