6feab533ce0e597fa3c3d30d04771d6ff365e4d3
glennhenry <glennhenrybdg05@gmail.com>
2025-11-09 14:10:42 +0700

    Update CSS

--- src/assets/headlinks.ts
+++ src/assets/headlinks.ts
@@ -20,5 +20,5 @@ export const fontHeadTags = [
       rel: "stylesheet",
       href: "https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap",
     },
-  },
+  }
 ] as const;
